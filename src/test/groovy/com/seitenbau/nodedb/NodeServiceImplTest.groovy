ae4b8608f9d1cac3f7dafa447d5bb800873a39f2
Christian Baranowski <develop@pro-pix.de>
2011-11-09 22:22:15 +0100

    add contiperf to test and to the pom as dependency

--- src/test/groovy/com/seitenbau/nodedb/NodeServiceImplTest.groovy
+++ src/test/groovy/com/seitenbau/nodedb/NodeServiceImplTest.groovy
@@ -1,17 +1,25 @@
 package com.seitenbau.nodedb;
 
 
+import org.databene.contiperf.PerfTest
+import org.databene.contiperf.Required
+import org.databene.contiperf.junit.ContiPerfRule
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
+@PerfTest(invocations = 10)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/spring/core-context.xml")
 public class NodeServiceImplTest
 {
+
+    @Rule
+    public ContiPerfRule contiPerf = new ContiPerfRule();
 
     @Autowired
     NodeService nodeService
@@ -24,6 +32,7 @@ public class NodeServiceImplTest
     }
 
     @Test
+    @Required(max = 100, average = 50)
     void testGetAll() throws Exception
     {
         def nodes = nodeService.getAll()
